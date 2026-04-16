90b6c33d3df139e117820bfaea917f0f3497a737
istvan.polacsek <istvan.polacsek@uicentric.com>
2026-04-16 15:07:44 +0200

    feat(ui): dialog
    prepare popover dialog component

--- libs/ui/src/components/contact-dialog/ContactDialog.styles.tsx
+++ libs/ui/src/components/contact-dialog/ContactDialog.styles.tsx
@@ -13,11 +13,50 @@ export const ContactDialogStyled = styled.form(
 );
 
 export const ContactDialogActionsStyled = styled.div(
-  ({ theme }) => css`
+  () => css`
     display: flex;
     padding-block: 1.5rem 0;
     column-gap: 0.5rem;
     align-items: center;
     justify-content: end;
+  `,
+);
+
+export const ContactDialogPhotoEditStyled = styled.div(
+  () => css`
+    display: flex;
+    align-items: center;
+    column-gap: 1rem;
+  `,
+);
+
+export const ContactDialogPhotoInputStyled = styled.input(
+  () => css`
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    border: 0;
+    clip: rect(0 0 0 0);
+    clip-path: inset(100%);
+    white-space: nowrap;
+  `,
+);
+
+export const ContactDialogPhotoActionsStyled = styled.div(
+  () => css`
+    display: flex;
+    column-gap: 0.5rem;
+  `,
+);
+
+export const ContactDialogPhotoWrapperStyled = styled.div(
+  () => css`
+    max-width: 5.5rem;
+    max-height: 5.5rem;
+    border-radius: 100%;
+    overflow: hidden;
   `,
 );

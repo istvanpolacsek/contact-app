c4ee18caa09e6e7107f65ec7e8dd3a2a48096d6f
istvan.polacsek <istvan.polacsek@uicentric.com>
2026-04-16 10:40:05 +0200

    feat(ui): image
    prepare image component

--- libs/ui/src/components/image/Image.stories.tsx
+++ libs/ui/src/components/image/Image.stories.tsx
@@ -1,10 +1,20 @@
 import type { Meta, StoryObj } from '@storybook/nextjs';
 
 import Image from './Image';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+const ImageDecorator = styled.div(
+  () => css`
+    width: 10rem;
+    height: 10rem;
+  `,
+);
 
 const meta = {
   component: Image,
   parameters: { layout: 'centered' },
+  decorators: [(Story) => <ImageDecorator>{Story()}</ImageDecorator>],
 } satisfies Meta<typeof Image>;
 
 export default meta;
@@ -13,7 +23,14 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    src: 'src',
+    src: 'https://picsum.photos/256',
+    alt: 'alt',
+  },
+};
+
+export const Placeholder: Story = {
+  args: {
+    src: 'bad-url',
     alt: 'alt',
   },
 };

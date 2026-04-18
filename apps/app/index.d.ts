575c0c0ab682a80314329522a8f06e64f848e65a
istvan.polacsek <istvan.polacsek@uicentric.com>
2026-04-18 12:35:05 +0200

    feat(app): build
    make sure next build runs and is stored in the appropriate dir

--- apps/app/index.d.ts
+++ apps/app/index.d.ts
@@ -1,6 +1,12 @@
+import type { Theme as CustomTheme } from '@contact-app/theme';
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '*.svg' {
   const content: any;
   export const ReactComponent: any;
   export default content;
+}
+
+declare module '@emotion/react' {
+  export type Theme = CustomTheme;
 }
